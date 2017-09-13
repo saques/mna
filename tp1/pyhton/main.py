c122e55eaec095dcb6fc35559d6f80ed682fcca8
Pedro Balaguer <pbalaguer@itba.edu.ar>
2017-09-13 19:54:20 -0300

    KPCA algorithm not working yet

--- tp1/pyhton/main.py
+++ tp1/pyhton/main.py
@@ -1,3 +1,4 @@
+from lib import *
 import numpy as np
 import cv2
 import os.path
@@ -61,10 +62,13 @@ def print_images(mats, name='image', time=1000):
 IMG_HEIGHT = 112
 IMG_WIDTH = 92
 
+trnperper = 5
+trnno = trnperper*40  # training number
 
 # Load all images from database (located in '../faces')
 # db = loadImages(["../faces/s1","../faces/s2","../faces/s3","../faces/s4","../faces/s5"])
-db = load_images(["../faces"])
+# db = load_images(["../faces"])
+db = load_images_2(trnperper)
 
 # Calculating mean of all faces
 mean_face = db.mean(0)
@@ -76,17 +80,35 @@ print_image(np.reshape(mean_face, [112, 92]), "mean", 500)
 
 # Calculating pseudo-covariance matrix and calculating its eigenvalues
 # and eigenvectors.
-pseudo = np.dot(dbc, dbc.transpose())
-eigenvalues, eigenvectors = np.linalg.eig(pseudo)
+# pseudo = np.dot(dbc, dbc.transpose())
+# eigenvalues, eigenvectors = np.linalg.eig(pseudo)
+
+
+# KERNEL: polinomial de grado degree
+degree = 2
+K = (np.dot(dbc, dbc.T) / trnno + 1) ** degree
+# K = (K + K.T)/2.0
+
+# esta transformacion es equivalente a centrar las imagenes originales...
+unoM = np.ones([trnno, trnno]) / trnno
+K = K - np.dot(unoM, K) - np.dot(K, unoM) + np.dot(unoM, np.dot(K, unoM))
+
+eigenvalues, eigenvectors = np.linalg.eigh(K)
+eigenvalues = np.flipud(eigenvalues)
+eigenvectors = np.fliplr(eigenvectors)
+
+for col in range(eigenvectors.shape[1]):
+    eigenvectors[:,col] = eigenvectors[:,col]/np.sqrt(np.abs(eigenvalues[col]))
+
 
 # Calculating eigenfaces using the best eigenvectors (ranked by their
 # eigenvalues). This is made by projecting the original mean-substracted images.
-eigenfaces = []
-for x in range(eigenvectors.shape[0]):
-    eigenfaces.append(np.dot(dbc.transpose(), eigenvectors[x]))
+# eigenfaces = []
+# for x in range(eigenvectors.shape[0]):
+#     eigenfaces.append(np.dot(dbc.transpose(), eigenvectors[x]))
+# eigenfaces = np.stack(eigenfaces)
 
-eigenfaces = np.stack(eigenfaces)
-
+eigenfaces = np.dot(K.T, eigenvectors)
 
 def find_closest_face(id, eigenfaces_arg, normalized_database):
     closest_faces = []
@@ -113,9 +135,34 @@ def calculate_img_weights(eigenfaces_arg, face):
     projected = np.stack(projected)
     return projected
 
+def find_closest_face(proyected_face, eigenfaces_arg, normalized_database):
+    closest_faces = []
+    face_weights = norm_img_weights(eigenfaces_arg, proyected_face)
+    for x in range(normalized_database.shape[0]):
+        val = norm_img_weights(eigenfaces_arg, normalized_database[x]) - face_weights
+        val = np.linalg.norm(val, 2)
+        closest_faces.append((x, val))
+
+    closest_faces = sorted(closest_faces, key=lambda x: x[1])
+    return closest_faces
+
+
+unoML = np.ones([1, trnno]) / trnno
+Ktest = (np.dot(dbc[43], dbc.T) / trnno + 1) ** degree
+Ktest = Ktest - np.dot(unoML, K) - np.dot(Ktest, unoM) + np.dot(unoML, np.dot(K, unoM))
+imtstproypre = np.dot(Ktest, eigenvectors)
+
+
+print eigenfaces.shape
+print imtstproypre
+
+# print find_closest_face(imtstproypre, eigenfaces, db)
 
 # Look for the nearest face
-print "CLOSEST FACES"
-closest_faces = find_closest_face(43, eigenfaces, dbc)
-print closest_faces
+# print "CLOSEST FACES"
+# closest_faces = find_closest_face(43, eigenfaces, dbc)
+# print closest_faces
+#
+# print_images([np.reshape(db[closest_faces[1][0]],[112,92]),
+#               np.reshape(db[43], [112, 92])], time=0)
 
