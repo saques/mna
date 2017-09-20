0fa64de93a988eb52a78ed47fc49f4db4311d9e2
Pedro Balaguer <pbalaguer@itba.edu.ar>
2017-09-20 01:11:58 -0300

    Runtime face recognizion working fine

--- tp1/pyhton/cam.py
+++ tp1/pyhton/cam.py
@@ -4,6 +4,7 @@ import cv2
 from kpca import KPCA
 from sklearn import svm
 import numpy as np
+from collections import defaultdict
 
 HEIGHT = 480
 WIDTH = 640
@@ -11,13 +12,16 @@ WIDTH = 640
 HEIGHT_PGM = 112
 WIDTH_PGM = 92
 
+names = {0: 'Pedro',
+         1: 'Alejo'}
+names = defaultdict(lambda: "Unknown", names)
+
 
 def rotate_img(img, angle):
     loc = img.get_rect().center
     rot_sprite = pygame.transform.rotate(img, angle)
     rot_sprite.get_rect().center = loc
     return rot_sprite
-
 
 
 pygame.camera.init()
@@ -51,27 +55,29 @@ while True:
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(img, "Hello World!!!", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), thickness=2)
-        cv2.putText(img, "Hello World!!!", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), thickness=1)
+
+        ymin = max(y - int(h * 0.3), 0)
+        ymax = min(y + int(h * 1.5), HEIGHT)
+        ratio = (ymax - ymin) / float(HEIGHT_PGM)
+
+        x_center = int(x + w / 2)
+        xmin = max(int(x_center - (WIDTH_PGM / 2) * ratio), 0)
+        xmax = min(int(x_center + (WIDTH_PGM / 2) * ratio), WIDTH)
+        face = gray[ymin:ymax, xmin:xmax]  # Why upside down?
+
+        face = cv2.resize(face, (92, 112))
+
+        class_predicted = clf.predict(kpca.project_images(np.ravel(face)))
+        cv2.putText(img, names[class_predicted[0]], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), thickness=2)
+        cv2.putText(img, names[class_predicted[0]], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), thickness=1)
 
     cv2.imshow("Video", img)
 
     if cv2.waitKey(1) & 0xFF == ord('s'):
         print "Saving image..."
 
-        ymin = max(y-int(h * 0.3), 0)
-        ymax = min(y+int(h * 1.5), HEIGHT)
-        ratio = (ymax - ymin) / float(HEIGHT_PGM)
-
-        x_center = int(x + w/2)
-        xmin = max(int(x_center - (WIDTH_PGM/2)*ratio), 0)
-        xmax = min(int(x_center + (WIDTH_PGM/2)*ratio), WIDTH)
-        face = gray[ymin:ymax, xmin:xmax]  # Why upside down?
-
-        face = cv2.resize(face, (92, 112))
-        cv2.imwrite("saved_face.pgm", face)
-
-        print clf.predict(kpca.project_images(np.ravel(face)))
+        class_predicted = clf.predict(kpca.project_images(np.ravel(face)))
+        print names[class_predicted[0]]
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print "Closing program..."
