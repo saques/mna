0fe524c9f922c6823254da659953bd408c0a1511
masaques <msaques@itba.edu.ar>
2017-10-25 13:47:20 -0300

    iterative fft. Same order but 3.2*quicker

--- tp2/python/lib.py
+++ tp2/python/lib.py
@@ -1,8 +1,9 @@
 import numpy as np
+import time
 
 
 #Brute force implementation
-def fft_bf(x):
+def dft(x):
     N = len(x)
     ans = np.empty((N,)).astype(complex)
 
@@ -35,3 +36,62 @@ def fft_ct(x):
     if not (N & (N-1)) == 0:
         raise ValueError("Length must be a power of 2")
     return fft_ct_1(x, N)
+
+
+def ifft(x):
+    N = len(x)
+
+    if not (N & (N-1)) == 0:
+        raise ValueError("Length must be a power of 2")
+
+    A = bit_reverse(x).astype(complex)
+
+    for s in range(1, 1+int(np.log(N)/np.log(2))):
+        m = 1 << s
+        Wm = np.exp(-2*np.pi*1j/m)
+        for k in range(0, N, m):
+            W = 1
+            for j in range(0, m/2):
+                t = W*A[k + j + m/2]
+                u = A[k + j]
+                A[k + j] = u + t
+                A[k + j + m/2] = u - t
+                W = W * Wm
+    return A
+
+
+def bit_reverse(x):
+    N = len(x)
+    if not (N & (N-1)) == 0:
+        raise ValueError("Length must be a power of 2")
+    ans = np.zeros((N,))
+    ans[0] = x[0]
+    ans[N-1] = x[N-1]
+    bit_reverse_1(ans, x, N/2, 1, N/2, 1, N)
+    return ans
+
+
+def bit_reverse_1(ans, x, pos, val, n, inc, N):
+    if pos >= N or pos <= 0 or n == 0:
+        return
+    ans[pos] = x[val]
+    bit_reverse_1(ans, x, pos-n/2, val+inc, n/2, inc*2, N)
+    bit_reverse_1(ans, x, pos+n/2, val+inc*2, n/2, inc*2, N)
+
+
+
+q = np.random.rand(32768/2/2,)
+
+t0 = time.time()
+fft_ct(q)
+print "Recursive: %f" % (time.time()-t0)
+
+t0 = time.time()
+ifft(q)
+print "Iterative: %f" % (time.time()-t0)
+
+t0 = time.time()
+np.fft.fft(q)
+print "Numpy: %f" % (time.time()-t0)
+
+# print np.allclose(fft_ct(q),ifft(q)) and np.allclose(ifft(q),np.fft.fft(q))
---
+++ tp2/python/onlygreens.py
@@ -0,0 +1,56 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from lib import fft_ct
+
+
+cap = cv2.VideoCapture('sample5.mp4')
+
+if not cap.isOpened():
+    print("No lo pude abrir")
+    exit(0)
+
+
+
+length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+print width
+print height
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+NAME = 'sample5og.avi'
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+out = cv2.VideoWriter(NAME, fourcc, fps, (width,height))
+
+
+k = 0
+while (cap.isOpened()):
+    ret, frame = cap.read()
+
+    if ret == True:
+        #frame[:, :, 0] -= frame[:, :, 0]
+        frame[:, :, 1] -= frame[:, :, 1]
+        frame[:, :, 2] -= frame[:, :, 2]
+        out.write(frame)
+    else:
+        break
+    k = k + 1
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
